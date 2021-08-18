bfe62d3403bee2106555b3365584364751d5a605
VIsaev <VIsaev@lanit.ru>
2021-08-18 17:33:15 +0300

    - added getter, setter annotation

--- src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/repository/entity/Custom.java
+++ src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/repository/entity/Custom.java
@@ -1,6 +1,8 @@
 package ru.lanit.bpm.coffeelogger.bot.longpolling.repository.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Basic;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
 public class Custom {
     @Id
     @GeneratedValue(generator = "system-uuid")
@@ -18,5 +22,5 @@ public class Custom {
     @Basic
     private String type;
     @Basic
-    private String price;
+    private int price;
 }
--- src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/repository/entity/Operator.java
+++ src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/repository/entity/Operator.java
@@ -1,6 +1,8 @@
 package ru.lanit.bpm.coffeelogger.bot.longpolling.repository.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
 public class Operator {
     @Id
     private String id;
