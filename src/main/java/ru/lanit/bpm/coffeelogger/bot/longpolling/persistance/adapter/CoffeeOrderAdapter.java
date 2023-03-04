c47a54e0ce91cecb8319c6b1b57d3d0cf424b9ea
VIsaev <vladviv91@gmail.com>
2023-03-04 14:09:29 +0300

    - added stats feature

--- src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/persistance/adapter/CoffeeOrderAdapter.java
+++ src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/persistance/adapter/CoffeeOrderAdapter.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import ru.lanit.bpm.coffeelogger.bot.longpolling.persistance.repository.CoffeeOrderRepository;
 import ru.lanit.bpm.coffeelogger.bot.longpolling.persistance.entity.Coffee;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,16 +16,24 @@ public class CoffeeOrderAdapter {
 
     public void createOrder(HashMap<String, String> coffeeOrder, Long userId) {
         var coffee = createCoffeeEntity(coffeeOrder, userId);
-        save(coffee);
+        repository.save(coffee);
+    }
+
+    public List<Coffee> getWeekStats(Long userId) {
+        return repository.findCoffeesByUserIdAndCreateDateTimeAfter(userId, computeAfterDate(7L));
+    }
+
+    public List<Coffee> getMontStats(Long userId) {
+        return repository.findCoffeesByUserIdAndCreateDateTimeAfter(userId, computeAfterDate(31L));
+    }
+
+    public void repeatLastOrder(Long userId) {
+
     }
 
     // ===================================================================================================================
     // = Implementation
     // ===================================================================================================================
-
-    private void save(Coffee coffeeOrder) {
-        repository.save(coffeeOrder);
-    }
 
     private static Coffee createCoffeeEntity(HashMap<String, String> coffeeOrder, Long userId) {
         var coffee = new Coffee();
@@ -33,5 +43,9 @@ public class CoffeeOrderAdapter {
         coffee.setSize(Float.parseFloat(coffeeOrder.get("CoffeeSize")));
         coffee.setUserId(userId);
         return coffee;
+    }
+
+    private LocalDateTime computeAfterDate(Long daysCount) {
+        return LocalDateTime.now().minusDays(daysCount);
     }
 }
--- src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/service/BotService.java
+++ src/main/java/ru/lanit/bpm/coffeelogger/bot/longpolling/service/BotService.java
@@ -2,6 +2,7 @@ package ru.lanit.bpm.coffeelogger.bot.longpolling.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.lanit.bpm.coffeelogger.bot.longpolling.persistance.adapter.CoffeeOrderAdapter;
@@ -13,19 +14,33 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor
 public class BotService {
     private final CoffeeOrderAdapter coffeeOrderAdapter;
+    private final MessageCreatorService messageCreatorService;
 
+    @Transactional
     public SendMessage handleMessage(Update update) {
         var message = new SendMessage();
         var incomingMessage = update.getMessage().getText();
+        var userId = update.getMessage().getFrom().getId();
         message.setChatId(update.getMessage().getChatId().toString());
 
-        if (incomingMessage.equals("start")) {
-            message.setText(startMessageText());
+        if (incomingMessage.equals("start") || incomingMessage.equals("/start")) {
+            message.setText(messageCreatorService.createStartMessageText());
         } else if (incomingMessage.startsWith("[CoffeeOrder]")) {
             coffeeOrderAdapter.createOrder(
                     fillOrder(incomingMessage),
-                    update.getMessage().getFrom().getId());
+                    userId);
             message.setText("Order saved");
+        } else if (incomingMessage.equals("/week")) {
+            var orderList = coffeeOrderAdapter.getWeekStats(userId);
+            var messageText = messageCreatorService.createStatsMessageText(orderList);
+            message.setText(messageText);
+        } else if (incomingMessage.equals("/month")) {
+            var orderList = coffeeOrderAdapter.getMontStats(userId);
+            var messageText = messageCreatorService.createStatsMessageText(orderList);
+            message.setText(messageText);
+        } else if (incomingMessage.equals("/repeat")) {
+            coffeeOrderAdapter.repeatLastOrder(userId);
+            message.setText("Order repeated");
         } else {
             message.setText("Write start");
         }
@@ -48,10 +63,5 @@ public class BotService {
             coffeeOrder.put(keysMatcher.group(1).trim(), valueMatcher.group(1));
         }
         return coffeeOrder;
-    }
-
-    private String startMessageText() {
-        return "Скопируйте шаблон и заполните значения в <>:" +
-                "\n[CoffeeOrder]: CoffeeType: <>, CoffeeSize: <> мл, CoffeePrise: <>, CafeName: <>";
     }
 }
